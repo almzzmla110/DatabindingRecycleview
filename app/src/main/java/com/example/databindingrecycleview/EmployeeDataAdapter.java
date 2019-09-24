868e3c3ec1b658a71cfd64fad62cf0d06c992688
Sanika Vanjari <sanikavanjari94@gmail.com>
2019-09-24 18:03:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databindingrecycleview/EmployeeDataAdapter.java
@@ -0,0 +1,56 @@
+package com.example.databindingrecycleview;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.databindingrecycleview.databinding.EmployeeRowLayoutBinding;
+
+import java.util.ArrayList;
+
+public class EmployeeDataAdapter extends RecyclerView.Adapter<EmployeeDataAdapter.EmployeeViewHolder> {
+
+    private ArrayList<Employee> employees;
+
+    @NonNull
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        EmployeeRowLayoutBinding employeeRowLayoutBinding = DataBindingUtil
+                .inflate(LayoutInflater.from(parent.getContext()), R.layout.employee_row_layout, parent, false);
+        return new EmployeeViewHolder(employeeRowLayoutBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
+        Employee currentEmp = employees.get(position);
+        holder.employeeRowLayoutBinding.setEmployee(currentEmp);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (employees != null) {
+            return employees.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employees) {
+        this.employees = employees;
+        notifyDataSetChanged();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        private EmployeeRowLayoutBinding employeeRowLayoutBinding;
+
+        public EmployeeViewHolder(@NonNull EmployeeRowLayoutBinding employeeRowLayoutBinding) {
+            super(employeeRowLayoutBinding.getRoot());
+
+            this.employeeRowLayoutBinding = employeeRowLayoutBinding;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/databindingrecycleview/EmployeeDataService.java
@@ -0,0 +1,9 @@
+package com.example.databindingrecycleview;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EmployeeDataService {
+    @GET("users/?per_page=12&amp;page=1")
+    Call<EmployeeDBResponse> getEmployees();
+}
